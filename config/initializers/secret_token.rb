58ac99e56e6c8c77017016d37639a7b545fd5a01
Amy Ton <amyhton@gmail.com>
2013-10-19 16:14:14 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GloveSlap::Application.config.secret_key_base = 'f1648771428dd7fbef4cf60a6fabc588f14131777829fb1032aac9f6ba7103e25289c789988329f388dcc111d8f66b170006d83c0cfdbb981e66e6b438730a7b'
