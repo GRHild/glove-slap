70c67a8839d1054b6f462dfcb8845fd66a23037c
Amy Ton <amyhton@gmail.com>
2013-10-23 14:33:53 -0500

    Add testing for creating a new user

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,10 +2,26 @@ require 'spec_helper'
 
 describe UserController do
   describe "POST api/user/location" do
-    it "updates the user's location" do
+
+    # when a user first visits the app
+
+    it "creates a new user" do
+      # Make the post, assign location to phone
+      post :update_location, phone_id: "Amy", location: "78705"
+      # Grab the new user from the database
+      user = User.first
+      # Make sure that the user's location was updated
+      expect(user).to_not be_nil
+      expect(user.phone_id).to eq("Amy")
+      expect(user.location).to eq("78705")
+    end
+
+    # when a user revisits the app
+
+    it "updates an existing user's location" do
       # Create user before POSTing
       user_id = User.create(phone_id: "Amy", location: nil).id
-      # Make the post
+      # Make the post, assign location to phone
       post :update_location, phone_id: "Amy", location: "78705"
       # Grab the user from the database
       user = User.find(user_id)
