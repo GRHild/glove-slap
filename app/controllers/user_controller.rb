c8a54c4b4f8d50c5edf7c578835cfec3fc947396
Amy Ton <amyhton@gmail.com>
2013-10-26 14:12:57 -0500

    add update_location action to create new user, find user in db and update to new location, and find all users in the location

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,23 @@
 class UserController < ApplicationController
   def update_location
-    @user = User.create(phone_id: params[:id], location: params[:location])
+    # create new user with phone_id and location
+    if User.where(phone_id: params[:phone_id]).blank?
+      @user = User.create(phone_id: params[:phone_id], location: params[:location])
+    else
+      # find user in the database
+      @user = User.where(phone_id: params[:phone_id]).first
+      # update to the new location
+      @user.update(location: params[:location])
+    end
+
+    # find all users in the location
     @nearby_users = User.all
+
+    # returns nearby_users in json
     render :json => @nearby_users.to_json , :status => 200
+  end
+
+  def slap
+    #
   end
 end
