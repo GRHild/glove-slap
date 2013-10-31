a16e01851bc240e40c9ad57d5e3764cc7a8d8fb0
Amy Ton <amyhton@gmail.com>
2013-10-31 13:51:47 -0500

    Re-add correct filepicker column to Users table

--- db/migrate/20131031025717_add_name_of_attr_for_filepicker_url_to_users.rb
+++ db/migrate/20131031025717_add_name_of_attr_for_filepicker_url_to_users.rb
@@ -1,4 +1,5 @@
 class AddNameOfAttrForFilepickerUrlToUsers < ActiveRecord::Migration
   def change
+    add_column :users, :filepicker_url, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131027022942) do
+ActiveRecord::Schema.define(version: 20131031025717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,13 +31,11 @@ ActiveRecord::Schema.define(version: 20131027022942) do
     t.string   "phone_id"
     t.string   "location"
     t.string   "nickname"
-<<<<<<< Updated upstream
-    t.string   "rank"
-    t.text     "avatar_url"
-=======
->>>>>>> Stashed changes
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rank"
+    t.string   "avatar_url"
+    t.string   "filepicker_url"
   end
 
 end
