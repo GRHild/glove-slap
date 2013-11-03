269d39cb9d8a09aa00caaa9b11e5456e3802e038
Amy Ton <amyhton@gmail.com>
2013-11-03 13:00:46 -0600

    Create new migrations to add latitude and longitude to Users and remove location from Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131103022530) do
+ActiveRecord::Schema.define(version: 20131103184724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,12 +29,13 @@ ActiveRecord::Schema.define(version: 20131103022530) do
 
   create_table "users", force: true do |t|
     t.string   "phone_id"
-    t.string   "location"
     t.string   "nickname"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "rank"
     t.string   "email"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
