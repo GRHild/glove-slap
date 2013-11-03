6ca5376eaa7f309b96958610a528aad9c2276126
Amy Ton <amyhton@gmail.com>
2013-11-03 00:52:27 -0500

    Passed testing to return the user's record and nearby users

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -27,16 +27,29 @@ describe UserController do
       expect(user.location).to eq("78705")
     end
 
-    it "renders nearby users" do
-      # Create user before POSTing
-      # user_id = User.create(phone_id: "Amy", location: nil).id
+    it "returns the user's record and nearby users" do
+      # Create a few users before POSTing
+      User.create(phone_id: "Bob", location: nil).id
+      User.create(phone_id: "Carl", location: nil).id
+      User.create(phone_id: "Dan", location: nil).id
+
       # Make the post, assign location to phone
       post :update_location, phone_id: "Amy", location: "78705"
-      puts response.body
-      # Grab the user from the database
-      # user = User.find(user_id)
-      # Make sure that the user's location was updated
-      # expect(user.location).to eq("78705")
+      resp = JSON.parse(response.body)
+
+      # Expect a user record
+      expect(resp['user']).to_not be_nil
+      expect(resp['user']['id']).to_not be_nil
+      expect(resp['user']['phone_id']).to eq("Amy")
+      expect(resp['user']['location']).to eq("78705")
+
+      # Expect a user record
+      expect(resp['nearby_users']).to_not be_nil
+      expect(resp['nearby_users'].count).to eq(4)
+    end
+
+    it "shows the user info" do
+      # user = User.find(3)
     end
   end
 
