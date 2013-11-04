f68757beb6c648e521ef0340f4be6351e5ce96ac
Amy Ton <amyhton@gmail.com>
2013-11-04 16:11:07 -0600

    Add update_gravatar controller method

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -23,4 +23,14 @@ class UserController < ApplicationController
 
     render :json => @user.to_json , :status => 200
   end
+
+  def update_gravtar
+    @user = User.find_by(phone_id: params[:phone_id])
+
+    if not @user.email
+      @user.update(email: params[:email])
+    end
+
+    render :json => @user.to_json , :status => 200
+  end
 end
